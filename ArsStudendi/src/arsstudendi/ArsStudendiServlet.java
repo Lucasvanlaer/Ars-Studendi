62c75ab1ef80bfc7dc528e8416b909f2cd9946f9
r0257393 <r0257393@geel.cs.kotnet.kuleuven.be.>
2012-10-26 16:45:25 +0200

    gfsdg

---
+++ ArsStudendi/src/arsstudendi/ArsStudendiServlet.java
@@ -0,0 +1,11 @@
+package arsstudendi;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class ArsStudendiServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
