c72bc3732720931ccda0470474562f9f19773129
michielmeertens <michielmeertens@10.8.92.236>
2012-11-09 17:11:48 +0100

    registercontroller

--- ArsStudendi/src/Controllers/RegisterController.java
+++ ArsStudendi/src/Controllers/RegisterController.java
@@ -1,5 +1,18 @@
 package Controllers;
 
+import java.util.List;
+
+import DomainModel.*;
+import DomainModel.StudyProgram;
+
 public class RegisterController {
 
+	public RegisterController (){
+		
+	}
+	public void makeStudent(StudyProgram studyPorgram, String studentName, long studentID, String password, List<Course> courses, String newEmailAdress)
+	{
+	Student student = new Student(studyPorgram,studentName,studentID,password,courses,newEmailAdress);
+	
+	}
 }
