662bbc1d39dda2996f7917af2076c5a40a8d3dfa
Nils <Nils@10.8.72.137>
2012-11-12 23:22:35 +0100

    Controller

--- ArsStudendi/src/Controllers/RegisterController.java
+++ ArsStudendi/src/Controllers/RegisterController.java
@@ -1,6 +1,6 @@
 package Controllers;
 
-import java.util.List;
+import java.util.*;
 import arsstudendi.*;
 import DomainModel.*;
 import DomainModel.StudyProgram;
@@ -13,13 +13,17 @@ public class RegisterController {
 	public boolean makeStudent(Long program, String studentName, long studentID, String password, List<String> StrCourses, String newEmailAdress)
 	{
 		boolean succeed = false;
+		Long studyP = Long.valueOf(program);
 		// een boolean terug geven of het emailadres geldig is
-	//if( de boolean true is ){
-		StudyProgram studyProgram = StudyProgramFinder.Find(program);
-		List<Course> courses = null;
+		StudyProgramFinder studyProgramFinder = new StudyProgramFinder();
+		//Tijdelijk wegens niet static
+		StudyProgram studyProgram = studyProgramFinder.getStudyProgram(studyP);
+		List<Course> courses = new ArrayList<Course>();
+		CourseFinder courseFinder = new CourseFinder();
+		//Tijdelijk wegens niet static
 		for(String strCourse: StrCourses){
 			Long courseID = Long.valueOf(strCourse);
-			Course course = CourseFinder.Find(courseID);
+			Course course = courseFinder.getCourse(courseID);
 			courses.add(course);	
 		}
 		
@@ -42,10 +46,12 @@ public class RegisterController {
 		if(email==null){return false;}else{return true;}
 	}
 	public boolean checkList(List<String> courseList){
-		List<Course> courses = null;
+		List<Course> courses = new ArrayList<Course>();
+		CourseFinder courseFinder = new CourseFinder();
+		//Tijdelijk wegens niet static
 		for(String strCourse: courseList){
 			Long courseID = Long.valueOf(strCourse);
-			Course course = CourseFinder.Find(courseID);
+			Course course = courseFinder.getCourse(courseID);
 			courses.add(course);	
 		}
 		if(courses.size()==0){return false;}else{return true;}
